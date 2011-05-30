03b9407cd9ef35299b538c438fa88b2e568f81ab
jeromekuptz <jeromekuptz@gmail.com>
2011-05-29 23:25:35 -0700

    applying look & feel patch from bug 40

--- src/main/java/com/rsshandler/Main.java
+++ src/main/java/com/rsshandler/Main.java
@@ -1,8 +1,17 @@
 package com.rsshandler;
 
+import javax.swing.UIManager;
+
 public class Main {
 
   public static void main(String[] args) throws Exception {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    }
+    catch (Exception e) {
+      // ignore and move on
+      e.printStackTrace();
+    }
     PodcastServer server = new PodcastServerImpl();
     Gui gui = new Gui();
     gui.setServer(server);
